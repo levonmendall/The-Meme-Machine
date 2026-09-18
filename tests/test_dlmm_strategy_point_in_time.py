5ba6c29fd0c5cbe99b0adb58ce63cad2828e62b4
levonmendall <levon_mendall@yahoo.com>
2026-09-18 06:44:04 -0700

    [qualification-build] Test removal inside DLMM strategy counterfactual

--- tests/test_dlmm_strategy_point_in_time.py
+++ tests/test_dlmm_strategy_point_in_time.py
@@ -2,8 +2,9 @@ import unittest
 
 from meme_machine.dlmm_tape import reconstruct
 from meme_machine.dlmm_paper import ENTRY_COST, EXIT_COST
-from tests.dlmm_strategy_point_in_time import _bidask_bps, evaluate, regime_features
-from tests.test_dlmm_tape import interval
+from tests.dlmm_strategy_point_in_time import (_bidask_bps,_deposit,_replay,evaluate,regime_features)
+from tests.test_dlmm_tape import interval,encode_state,remove_liquidity_transaction
+from tests.dlmm_support import snapshot
 
 
 class DlmmPointInTimeResearch(unittest.TestCase):
@@ -23,6 +24,29 @@ class DlmmPointInTimeResearch(unittest.TestCase):
         self.assertEqual(result['fixed_cost_lamports'],ENTRY_COST+EXIT_COST)
         self.assertIn('pnl_lamports',result)
         self.assertIn('range_hit',result)
+
+    def test_external_removal_preserves_counterfactual_position_share_order(self):
+        start=snapshot();start['kind']='real'
+        state=__import__('meme_machine').dlmm.validate(start,100)
+        post,removed,tx=remove_liquidity_transaction(
+            state,bid=-1,slot=101,now=101,signature='remove-research')
+        end=encode_state(start,post,102,102)
+        sigs=[
+            dict(signature='remove-research',slot=101,transactionIndex=7,
+                 err=None,confirmationStatus='finalized'),
+            dict(signature='anchor',slot=99,transactionIndex=2,
+                 err=None,confirmationStatus='finalized')]
+        tape=reconstruct(
+            state,end,sigs,{'remove-research':tx},102,
+            [100,2**31-1,2**31-1])
+        position=_deposit(state,'foundation_spot',2)
+        before_supply=position['virtual']['bins']['-1']['supply']
+        replayed=_replay(position,tape)
+        self.assertEqual(
+            replayed['virtual']['bins']['-1']['supply'],
+            before_supply-removed['share'])
+        self.assertEqual(
+            replayed['shares']['-1'],position['shares']['-1'])
 
     def test_regime_features_use_only_warmup_tape(self):
         _start,state,end,sigs,txs=interval()
