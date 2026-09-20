ea2b0ea47fbf2d31e8bd6a212383da1efbfd1bc6
levonmendall <levon_mendall@yahoo.com>
2026-09-20 07:09:55 -0700

    [execution-cert] pin Pump certification policy hash

--- tests/test_pump_acceleration_policy_identity.py
+++ tests/test_pump_acceleration_policy_identity.py
@@ -2,7 +2,7 @@ import unittest
 
 from meme_machine.pump_acceleration_strategy import policy_hash
 
-FROZEN="b273bc6be47d4f5a65f39d5d4f616777e02cbe547e19b7a07b0fefe23970c246"
+FROZEN="bb2631d83f5be287a0afc01dfc6d7a4da8b7086ae0afd09dfbf27df6d9d66a6e"
 
 
 class FrozenPolicyIdentityTests(unittest.TestCase):
