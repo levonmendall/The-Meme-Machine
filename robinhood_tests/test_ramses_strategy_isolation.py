4f34cfec76b8f354488abc33360e9bcf0074fcca
levonmendall <levon_mendall@yahoo.com>
2026-09-24 21:22:05 -0700

    Canonicalize Ramses admission fixture

--- robinhood_tests/test_ramses_strategy_isolation.py
+++ robinhood_tests/test_ramses_strategy_isolation.py
@@ -6,7 +6,6 @@ import unittest
 
 from robinhood_research import BoundaryError
 from robinhood_research.ramses_strategy import (
-    ACTIVE_MODE,
     POLICY_HASH,
     STRATEGY_DOMAIN,
     STRATEGY_VERSION,
@@ -24,7 +23,7 @@ class RamsesStrategyIsolationTests(unittest.TestCase):
 
     def _decision(self, capital=100):
         return dict(
-            mode=ACTIVE_MODE,
+            mode="active_wide_maker",
             qualified=True,
             allocation_authority=False,
             strategy_domain=STRATEGY_DOMAIN,
